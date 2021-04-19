cb28f520ba6ebe1186ed7a873a6c4fbf2c08d486
Lukasz Raczylo <lukasz@raczylo.com>
2021-04-19 13:34:13 +0100

    Change $HOME settings for skaffold.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 PATH="$PATH:/srv/google-cloud-sdk/bin"
+HOME="/srv"
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS --project $GCP_PROJECT
 gcloud config set project $GCP_PROJECT
 gcloud container clusters get-credentials $GCP_CLUSTER --region $GCP_REGION
